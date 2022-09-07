acf03c906210f2612df5fec5e128f0ae1494e6ba
Polina Grigorovich <grigorovichpk@mail.ru>
2022-09-07 17:18:35 +0300

    Initial commit

---
+++ src/main/java/com/example/springcourse/Project3RESTForSensor/Project3RestForSensorApplication.java
@@ -0,0 +1,13 @@
+package com.example.springcourse.Project3RESTForSensor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Project3RestForSensorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Project3RestForSensorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springcourse/Project3RESTForSensor/Project3RestForSensorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springcourse.Project3RESTForSensor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Project3RestForSensorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
